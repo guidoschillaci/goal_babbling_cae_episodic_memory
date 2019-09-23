920f9711ddd94de266acc29308d6e17c47421c34
Guido Schillaci <guido.schillaci@informatik.hu-berlin.de>
2019-09-23 18:01:54 +0200

    print, python3 version

--- scripts/models.py
+++ scripts/models.py
@@ -20,7 +20,7 @@ from sklearn.cluster import KMeans
 class Models():
 
 	def __init__(self):
-		print 'Models created'
+		print ('Models created')
 
 	def getLayerIndexByName(self, model, layername):
 		for idx, layer in enumerate(model.layers):
@@ -35,7 +35,7 @@ class Models():
 		test_pos = []
 		with gzip.open(file_name, 'rb') as memory_file:
 			memories = pickle.load(memory_file)
-			print 'converting data...'
+			print ('converting data...')
 			count = 0
 			for memory in memories:
 				image = memory['image']
@@ -83,7 +83,7 @@ class Models():
 		train_cmds = commands
 		test_pos = positions[indices]
 		train_pos = positions
-		print "number of test images: ", len(test_images)
+		print ("number of test images: ", len(test_images))
 
 		return train_images, test_images, train_cmds, test_cmds, train_pos, test_pos
 
@@ -99,7 +99,7 @@ class Models():
 		decoder=[]
 		if os.path.isfile(cae_file) and os.path.isfile(e_file) and os.path.isfile(d_file): # trained cae model already exists
 			# load convolutional autoencoder 
-			print 'Loading existing pre-trained autoencoder: ', cae_file
+			print ('Loading existing pre-trained autoencoder: ', cae_file)
 			K.clear_session()
 			autoencoder = load_model(cae_file)
 			# Create a separate encoder model
@@ -109,7 +109,7 @@ class Models():
 			for i in range(2,enc_layer_idx+1):
 				encoder_layer = autoencoder.layers[i](encoder_layer)
 			encoder = Model(encoder_inp, encoder_layer)
-			print encoder.summary()
+			print (encoder.summary())
 			# Create a separate decoder model
 			decoder_inp = Input(shape=(code_size,))
 			decoder_layer = autoencoder.layers[enc_layer_idx+1](decoder_inp)			
@@ -119,14 +119,14 @@ class Models():
 #			for i in range(-(enc_layer_idx-1),-0):
 #				decoder_layer = autoencoder.layers[i](decoder_layer)
 			decoder = Model(decoder_inp, decoder_layer)
-			print decoder.summary()
-			print 'Autoencoder loaded'
+			print (decoder.summary())
+			print ('Autoencoder loaded')
 		else:
-			print 'Could not find autoencoder files. Building and training a new one.'
+			print ('Could not find autoencoder files. Building and training a new one.')
 			autoencoder, encoder, decoder = self.build_autoencoder(code_size = code_size, image_size = image_size)
 			if train_offline:
 				if train_images is None:
-					print 'I need some images to train the autoencoder'
+					print ('I need some images to train the autoencoder')
 					sys.exit(1)
 				self.train_autoencoder(autoencoder, encoder, decoder, train_images, batch_size=batch_size, cae_epochs=epochs)
 		return autoencoder, encoder, decoder
@@ -137,13 +137,13 @@ class Models():
 
 		forward_model = []
 		if os.path.isfile(filename):
-			print 'Loading existing pre-trained forward model: ', filename
+			print ('Loading existing pre-trained forward model: ', filename)
 			forward_model = load_model(filename)
-			print 'Forward model loaded'
+			print ('Forward model loaded')
 		else:
-			print ' image_size load ' , image_size
+			print (' image_size load ' , image_size)
 			forward_model = self.build_forward_model(image_size, autoencoder, code_size = code_size)
-			print 'Forward model does not exist, yet. Built and compiled a new one'
+			print ('Forward model does not exist, yet. Built and compiled a new one')
 			if train:
 				if train_images is None or test_images is None or train_pos is None or test_pos is None:
 					'I need data for training the forward model'
@@ -156,13 +156,13 @@ class Models():
 
 		forward_model = []
 		if os.path.isfile(filename):
-			print 'Loading existing pre-trained forward code model: ', filename
+			print ('Loading existing pre-trained forward code model: ', filename)
 			forward_model = load_model(filename)
-			print 'Forward code model loaded'
+			print ('Forward code model loaded')
 		else:
-			print ' image_size load ' , image_size
+			print (' image_size load ' , image_size)
 			forward_model = self.build_forward_code_model(code_size = code_size)
-			print 'Forward model does not exist, yet. Built and compiled a new one'
+			print ('Forward model does not exist, yet. Built and compiled a new one')
 			if train:
 				if train_codes is None or test_codes is None or train_pos is None or test_pos is None:
 					'I need data for training the forward model'
@@ -174,12 +174,12 @@ class Models():
 		filename = directory + filename
 		# build inverse model
 		if os.path.isfile(filename):
-			print 'Loading existing pre-trained inverse model: ', filename
+			print ('Loading existing pre-trained inverse model: ', filename)
 			inverse_model = load_model(filename)
-			print 'Inverse model loaded'
+			print ('Inverse model loaded')
 		else:
 			inverse_model = self.build_inverse_model(image_size)			
-			print 'Inverse model does not exist, yet. Built and compiled a new one'
+			print ('Inverse model does not exist, yet. Built and compiled a new one')
 			if train:
 				if train_images is None or test_images is None or train_pos is None or test_pos is None:
 					'I need data for training the inverse model'
@@ -192,12 +192,12 @@ class Models():
 		filename = directory + filename
 		# build inverse model
 		if os.path.isfile(filename):
-			print 'Loading existing pre-trained inverse code model: ', filename
+			print ( 'Loading existing pre-trained inverse code model: ', filename)
 			inverse_model = load_model(filename)
-			print 'Inverse model loaded'
+			print ('Inverse model loaded')
 		else:
 			inverse_model = self.build_inverse_code_model(code_size = code_size)			
-			print 'Inverse model does not exist, yet. Built and compiled a new one'
+			print ('Inverse model does not exist, yet. Built and compiled a new one')
 			if train:
 				if train_codes is None or test_codes is None or train_pos is None or test_pos is None:
 					'I need data for training the inverse code model'
@@ -210,28 +210,28 @@ class Models():
 
 		kmeans = None
 		if os.path.isfile(filename):
-			print 'Loading existing KMeans...'
+			print ('Loading existing KMeans...')
 			kmeans = pickle.load(open(filename, 'rb'))
 
 		else: 
-			print 'Could not find KMeans file.'
+			print ('Could not find KMeans file.')
 
 			if train_images is None:
-				print 'I need an encoder and  some images to train the kMeans'
+				print ('I need an encoder and  some images to train the kMeans')
 				sys.exit(1)
 
 			# encoding test images
-			print 'Encoding train images...'
+			print ('Encoding train images...')
 			train_images_codes = encoder.predict(train_images)
 			code_size = len(train_images_codes[0])
 
-			print 'training k-means'
+			print ('training k-means')
 			#kmeans = MiniBatchKMeans(n_clusters=goal_som_size*goal_som_size,random_state=0,batch_size=batch_size)
 			kmeans = KMeans(n_clusters=goal_size*goal_size,random_state=0)
 	#		for i in xrange(0, len(train_images_codes), batch_size):
 	#			kmeans = kmeans.partial_fit(train_images_codes[i:i+batch_size])
 			kmeans = kmeans.fit(train_images_codes)
-			print 'saving k-means'
+			print ('saving k-means')
 			pickle.dump(kmeans, open(filename, 'wb'))
 
 		return kmeans
@@ -241,36 +241,36 @@ class Models():
 
 		goal_som = None
 		if os.path.isfile(filename):
-			print 'Loading existing trained SOM...'
+			print ('Loading existing trained SOM...')
 			h5f = h5py.File(filename,'r')
 			weights= h5f['goal_som'][:]
 			code_size = len(weights[0][0])
 			h5f.close()
-			print 'code_size read ' , code_size
+			print ('code_size read ' , code_size)
 			goal_som = MiniSom(goal_size, goal_size, code_size) 
 			goal_som._weights = weights
-			print len(weights)
-			print 'Goal SOM loaded! Number of goals: ', str(goal_size*goal_size)
+			print (len(weights))
+			print ('Goal SOM loaded! Number of goals: ', str(goal_size*goal_size))
 		else:
-			print 'Could not find Goal SOM files.'
+			print ('Could not find Goal SOM files.')
 			if encoder is None or train_images is None:
-				print 'I need an encoder and some sample images to train a new SOM!'
+				print ('I need an encoder and some sample images to train a new SOM!')
 				sys.exit(1)
-			print 'Creating a new one'
+			print ('Creating a new one')
 			# creating self-organising maps for clustering the image codes <> the image goals
 
 			# encoding test images
-			print 'Encoding train images...'
+			print ('Encoding train images...')
 			train_images_codes = encoder.predict(train_images)
 			code_size = len(train_images_codes[0])
 
 			goal_som = MiniSom(goal_size, goal_size, code_size, sigma = 0.5, learning_rate = 0.5) 
-			print 'Initialising goal SOM...'
+			print ('Initialising goal SOM...')
 			goal_som.random_weights_init(train_images_codes)
 
 			#plot_som_scatter( encoder, goal_som, train_images)
 
-			print 'som quantization error: ', goal_som.quantization_error(train_images_codes)
+			print ('som quantization error: ', goal_som.quantization_error(train_images_codes))
 			print("Training goal SOM...")
 			goal_som.train_random(train_images_codes, 100)  # random training
 
@@ -309,7 +309,7 @@ class Models():
 		encoded = Dense(code_size, name='encoded')(x) 	
 
 
-		print 'encoded shape ', encoded.shape
+		print  ('encoded shape ', encoded.shape)
 	#	x = Reshape(target_shape=(8,8,1))(encoded) #-12
 #		x = Dense(code_size, activation='relu')(encoded)
 #		x = Reshape(target_shape=(4, 4, 1))(x) #-12
@@ -331,7 +331,7 @@ class Models():
 			ims = ims*max_pool_size
 		decoded = Conv2D(channels, (conv_size, conv_size), activation = 'sigmoid', padding='same', name= 'decoded')(x)
 		
-		print 'decoded shape ', decoded.shape
+		print ('decoded shape ', decoded.shape)
 
 		autoencoder = Model(input_img, decoded)
 		autoencoder.compile(optimizer='adam', loss='mean_squared_error')
@@ -347,7 +347,7 @@ class Models():
 		decoder_inp = Input(shape=(code_size,))
 #		decoder_inp = Input(shape=encoded.output_shape)
 		enc_layer_idx = self.getLayerIndexByName(autoencoder, 'encoded')
-		print 'encoder layer idx ', enc_layer_idx
+		print ('encoder layer idx ', enc_layer_idx)
 		decoder_layer = autoencoder.layers[enc_layer_idx+1](decoder_inp)			
 		for i in range(enc_layer_idx+2, len(autoencoder.layers)):
 			decoder_layer = autoencoder.layers[i](decoder_layer)
@@ -365,7 +365,7 @@ class Models():
 		autoencoder.save( './models/autoencoder.h5')
 		encoder.save('./models/encoder.h5')
 		decoder.save('./models/decoder.h5')
-		print 'autoencoder trained and saved '
+		print ('autoencoder trained and saved ')
 
 
 	def train_autoencoder_on_batch(self, autoencoder, encoder, decoder, train_data, batch_size = 1, cae_epochs = 1):
@@ -375,10 +375,10 @@ class Models():
 						callbacks=[tensorboard_callback], verbose=1)
 		#inverse_model.fit(images, motor_cmd, epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)#, callbacks=[showLR()])
 		#inverse_model.train_on_batch(images, motor_cmd)
-		print 'autoencoder updated'
+		print ('autoencoder updated')
 
 	def build_forward_code_model(self, input_dim=2, code_size = 4):
-		print 'building forward code model...'
+		print ('building forward code model...')
 		#forward_model = Sequential()
 
 		# create fwd model layers, partly from autoencoder (decoder part
@@ -395,12 +395,12 @@ class Models():
 		sgd = optimizers.SGD(lr=0.0014, decay=0.0, momentum=0.8, nesterov=True)
 		#fwd_model.compile(optimizer='adadelta', loss='mean_squared_error')
 		fwd_model.compile(optimizer=sgd, loss='mean_squared_error')
-		print 'forward model'
+		print ('forward model')
 		fwd_model.summary()
 		return fwd_model
 
 	def build_forward_model(self, image_size, autoencoder, input_dim=2, output_channels=1, max_pool_size = 2, conv_size = 3, channels =1, code_size = 4):
-		print 'building forward model...'
+		print ('building forward model...')
 		#forward_model = Sequential()
 		enc_layer_idx = self.getLayerIndexByName(autoencoder, 'encoded')
 
@@ -421,7 +421,7 @@ class Models():
 		fwd_model = Model(cmd_fwd_inp, decoded)
 		fwd_model.compile(optimizer='adam', loss='mse')
 
-		print 'forward model'
+		print ('forward model')
 		fwd_model.summary()
 		return fwd_model
 
@@ -431,7 +431,7 @@ class Models():
 		forward_model.fit(positions, images, validation_data=[test_pos, test_img], epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True, callbacks=[tensorboard_callback])
 
 		forward_model.save('./models/forward_model.h5')
-		print 'Forward model trained and saved'
+		print ('Forward model trained and saved')
 
 	def train_forward_code_model(self, forward_model, positions, codes, test_pos, test_codes, batch_size=128, epochs = 1):
 		tensorboard_callback = TensorBoard(log_dir='./logs/fwd_code', histogram_freq=0, write_graph=True, write_images=True)
@@ -439,17 +439,17 @@ class Models():
 		forward_model.fit(positions, codes, validation_data=[test_pos, test_codes], epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True, callbacks=[tensorboard_callback])
 
 		forward_model.save('./models/forward_code_model.h5')
-		print 'Forward code model trained and saved'
+		print ('Forward code model trained and saved')
 
 	def train_forward_model_on_batch(self, forward_model, positions, images, batch_size=32, epochs = 1):
 		forward_model.fit(positions, images, epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)#, callbacks=[showLR()])
 		#forward_model.train_on_batch(positions, images)
-		print 'Forward model updated'
+		print ('Forward model updated')
 
 	def train_forward_code_model_on_batch(self, forward_model, positions, codes, batch_size=32, epochs = 1):
 		#tensorboard_callback = TensorBoard(log_dir='./logs/fwd_code', histogram_freq=0, write_graph=True, write_images=True)
 		forward_model.fit(positions, codes, epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)#, callbacks=[tensorboard_callback])
-		print 'Forward code model updated'
+		print ('Forward code model updated')
 
 
 	def build_inverse_model(self,  image_size, input_dim=2, max_pool_size = 2, conv_size = 3, channels=1):
